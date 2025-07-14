3d96d2b87de411eeaea5134e5bb0abb31a96e3d3
EaintHmoo <eainthmooaung@gmail.com>
2025-07-14 10:02:35 +0630

    remove home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -121,7 +121,7 @@ function MobileNavigation(
         </div>
         <nav className="mt-6">
           <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-            <MobileNavItem href="/about">About</MobileNavItem>
+            <MobileNavItem href="/">About</MobileNavItem>
             <MobileNavItem href="/skills">Skills</MobileNavItem>
             <MobileNavItem href="/projects">Projects</MobileNavItem>
             <MobileNavItem href="/services">Service</MobileNavItem>
@@ -166,7 +166,7 @@ function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg ring-1 shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-        <NavItem href="/about">About</NavItem>
+        <NavItem href="/">About</NavItem>
         <NavItem href="/skills">Skills</NavItem>
         <NavItem href="/projects">Projects</NavItem>
         <NavItem href="/services">Services</NavItem>
@@ -229,7 +229,7 @@ function Avatar({
   return (
     <Link
       href="/"
-      aria-label="Home"
+      aria-label="About"
       className={clsx(className, 'pointer-events-auto')}
       {...props}
     >
